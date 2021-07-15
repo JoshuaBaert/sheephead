48ba0b913085d344b2dd87d900bf6e2dfec7b631
Joshua Baert <developer@baert.io>
2021-07-15 15:10:24 -0700

    Fixed screensize, Added zero point cards, and added suits to fail

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,19 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { failTable, pointTable, trumpOrder } from './cards';
 
-const numberOfTrump = 14;
+const numberOfTrump = trumpOrder.length;
 
 @Component({
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
   styles: [`
     .trump-card {
-      font-size: calc(calc(100vh / ${numberOfTrump + 1}) * .90);
-      height: calc(calc(100vh / ${numberOfTrump}) - 1px);
+      font-size: calc(calc(var(--vh) / ${numberOfTrump + 1}) * .90);
+      height: calc(calc(var(--vh) / ${numberOfTrump}) - 1px);
     }
   `],
   template: `
-    <div class="main">
+    <div class="main" (dblclick)="onDoubleClick()">
       <div class="trump-container">
         <div id="trump-suits">
           <div *ngFor="let trump of trumpOrder" class="trump-card" [innerHTML]="formatCardText(trump)"></div>
@@ -39,119 +40,46 @@ const numberOfTrump = 14;
 
         <div class="fail-summary">
           <h2>Fail (Non Trump)</h2>
-          <div class="fail-cards">
-            <div *ngFor="let card of failTable">{{card.card}}</div>
+          <div class="fail-wrapper">
+            <div class="fail-cards">
+              <ng-template ngFor let-card [ngForOf]="failTable">
+                <span class="clubs"> &clubs;</span>
+
+                <span class="spades"> &spades;</span>
+
+                <span class="hearts"> &hearts;</span>
+
+                {{card.card}}</ng-template>
+            </div>
           </div>
         </div>
       </div>
     </div>
   `,
 })
-export class AppComponent {
-  trumpOrder = [
-    {
-      suit: 'clubs',
-      card: 'Q',
-      isFirstQueen: true,
-    },
-    {
-      suit: 'diams',
-      card: '7',
-    },
-    {
-      suit: 'spades',
-      card: 'Q',
-    },
-    {
-      suit: 'hearts',
-      card: 'Q',
-    },
-    {
-      suit: 'diams',
-      card: 'Q',
-    },
-    {
-      suit: 'clubs',
-      card: 'J',
-      isFirstJack: true,
-    },
-    {
-      suit: 'spades',
-      card: 'J',
-    },
-    {
-      suit: 'hearts',
-      card: 'J',
-    },
-    {
-      suit: 'diams',
-      card: 'J',
-    },
-    {
-      suit: 'diams',
-      card: 'A',
-    },
-    {
-      suit: 'diams',
-      card: '10',
-    },
-    {
-      suit: 'diams',
-      card: 'K',
-    },
-    {
-      suit: 'diams',
-      card: '9',
-    },
-    {
-      suit: 'diams',
-      card: '8',
-    },
-  ];
-  pointTable = [
-    {
-      card: 'A',
-      value: 11,
-    },
-    {
-      card: '10',
-      value: 10,
-    },
-    {
-      card: 'K',
-      value: 4,
-    },
-    {
-      card: 'Q',
-      value: 3,
-    },
-    {
-      card: 'J',
-      value: 2,
-    },
-  ];
-  failTable = [
-    {
-      card: 'A',
-    },
-    {
-      card: '10',
-    },
-    {
-      card: 'K',
-    },
-    {
-      card: '9',
-    },
-    {
-      card: '8',
-    },
-    {
-      card: '7',
-    },
-  ];
+export class AppComponent implements OnInit {
+  trumpOrder = trumpOrder;
+  pointTable = pointTable;
+  failTable = failTable;
+
 
   formatCardText(trump): string {
     return `<span class="suit ${trump.suit}">&${trump.suit};</span>`;
+  }
+
+  ngOnInit(): void {
+    window.addEventListener('resize', this.resize.bind(this));
+    this.resize();
+  }
+
+  resize(): void {
+    let vh = window.innerHeight;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+
+  onDoubleClick(): void {
+    console.log('double clicked');
+    if (document.fullscreen) document.exitFullscreen();
+    else document.documentElement.requestFullscreen();
   }
 }
---
+++ client/src/app/cards.ts
@@ -0,0 +1,34 @@
+export const trumpOrder = [
+  { suit: 'clubs', card: 'Q', isFirstQueen: true },
+  { suit: 'diams', card: '7' },
+  { suit: 'spades', card: 'Q' },
+  { suit: 'hearts', card: 'Q' },
+  { suit: 'diams', card: 'Q' },
+  { suit: 'clubs', card: 'J', isFirstJack: true },
+  { suit: 'spades', card: 'J' },
+  { suit: 'hearts', card: 'J' },
+  { suit: 'diams', card: 'J' },
+  { suit: 'diams', card: 'A' },
+  { suit: 'diams', card: '10' },
+  { suit: 'diams', card: 'K' },
+  { suit: 'diams', card: '9' },
+  { suit: 'diams', card: '8' },
+];
+
+export const pointTable = [
+  { card: 'A', value: 11 },
+  { card: '10', value: 10 },
+  { card: 'K', value: 4 },
+  { card: 'Q', value: 3 },
+  { card: 'J', value: 2 },
+  { card: '9 8 7', value: 0 },
+];
+
+export const failTable = [
+  { card: 'A' },
+  { card: '10' },
+  { card: 'K' },
+  { card: '9' },
+  { card: '8' },
+  { card: '7' },
+];
